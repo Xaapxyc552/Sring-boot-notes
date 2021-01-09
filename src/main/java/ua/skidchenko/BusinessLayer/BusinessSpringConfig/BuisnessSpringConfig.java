cdd3cb99e743a50720bee0fa7a3463945cac3d95
Xaapxyc552 <oleksandr.skidchenko@gmail.com>
2021-01-09 11:43:01 +0200

    Some commit

---
+++ src/main/java/ua/skidchenko/BusinessLayer/BusinessSpringConfig/BuisnessSpringConfig.java
@@ -0,0 +1,9 @@
+package ua.skidchenko.BusinessLayer.BusinessSpringConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BuisnessSpringConfig {
+
+
+}
---
+++ src/main/java/ua/skidchenko/Model/Identifiable.java
@@ -0,0 +1,7 @@
+package ua.skidchenko.Model;
+
+import java.io.Serializable;
+
+public interface Identifiable extends Serializable {
+    Long getId ();
+}
